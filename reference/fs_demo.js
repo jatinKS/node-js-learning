21297f89f485767ae81557fd3784caa9be89e782
Jatin Gaurav <jatin.soni007@gmail.com>
2020-06-16 22:12:58 +0530

    File System Module - append

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname,'test'),{},err=>{
-    if(err) throw err;
-    console.log('Folder created...');
-});
+// fs.mkdir(path.join(__dirname,'test'),{},err=>{
+//     if(err) throw err;
+//     console.log('Folder created...');
+// });
 
 fs.writeFile(
-    path.join(__dirname,'test','hello.txt'),'Hello Mr. File',err=>{
+    path.join(__dirname,'test','note.txt'),'Hello Mr. File.',err=>{
+    if(err) throw err;
+    console.log('File is written');
+}); 
+fs.appendFile(
+    path.join(__dirname,'test','note.txt'),'Hello again',err=>{
     if(err) throw err;
     console.log('File is written');
 });
